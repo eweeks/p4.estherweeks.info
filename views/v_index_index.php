52948e20449f1f9fa7c970f5e36e05b91977c9aa
Esther Weeks <esta.weeks@gmail.com>
2013-12-21 17:44:39 -0700

    minor change to text on index page

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,6 +1,6 @@
 <p>Project 4- Photo File</p>
 <p>Welcome to Photo File!</p>
 <p>Photo File is designed to assist in archiving photos. Use the database to organize, and manage
-photo files, including location, projects used in, and format. Add files to projects to keep
+photo files. Add files to projects to keep
 track of when and where photos were used.</p>
 <p>Registration and access available only through admin.</p>
