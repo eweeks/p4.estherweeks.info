c2d2a3bd7fc1d9c16ec504e00a319924486d2275
Esther Weeks <esta.weeks@gmail.com>
2013-12-21 18:45:16 -0700

    fixing Error to validate

--- views/v_projects_view.php
+++ views/v_projects_view.php
@@ -18,12 +18,12 @@
 		
 		<a  href="#fileModal<?=$project['date_start']?>"  title="Delete File" data-toggle="modal">Delete File</a>
 		<!--modal content for delete file -->
-			<div id="fileModal<?=$project['date_start']?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="fileModalLabel" aria-hidden="true">
+			<div id="fileModal<?=$project['date_start']?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="fileModalLabel<?=$project['date_start']?>" aria-hidden="true">
 				<div class="modal-dialog">
 					<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-							<h4 class="modal-title" id="fileModalLabel">Are You Sure?</h4>
+							<h4 class="modal-title" id="fileModalLabel<?=$project['date_start']?>">Are You Sure?</h4>
 						</div>
 						<div class="modal-body">
 							<p>Are you sure you want to delete this file?</p>
