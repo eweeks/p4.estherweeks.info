55e37d6310f51989825c87eb87370f5943995fec
Esther Weeks <esta.weeks@gmail.com>
2013-12-21 18:40:55 -0700

    fixing Error to validate

--- views/v_projects_view.php
+++ views/v_projects_view.php
@@ -16,9 +16,9 @@
 		<p>Stored:<?=$project['stored']?></p>
 		<p>Date of file:<?=$project['date_start']?> </p>
 		
-		<a  href="#fileModal<?=$project['name']?>"  title="Delete File" data-toggle="modal">Delete File</a>
+		<a  href="#fileModal<?=$project['date_start']?>"  title="Delete File" data-toggle="modal">Delete File</a>
 		<!--modal content for delete file -->
-			<div id="fileModal<?=$project['name']?>" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="fileModalLabel" aria-hidden="true">
+			<div id="fileModal<?=$project['date_start']?> class="modal fade" tabindex="-1" role="dialog" aria-labelledby="fileModalLabel" aria-hidden="true">
 				<div class="modal-dialog">
 					<div class="modal-content">
 						<div class="modal-header">
